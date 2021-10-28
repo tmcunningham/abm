b81658b3dc32eb4bbee46470f15c801d4ca1ce5d
tmcunningham <80322877+tmcunningham@users.noreply.github.com>
2021-10-28 15:10:45 +0100

    Create final_model.py and move README to main folder

---
+++ agentframework.py
@@ -0,0 +1,241 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 16 13:33:53 2021
+
+@author: Tom Cunningham
+"""
+import random
+import operator
+
+class Agent():
+    """
+    Define an instance of a single agent.
+    
+    Properties
+    ----------
+    x : int 
+        Agent's x co-ordinate.
+    y : int
+        Agent's y co-ordinate.
+    environment : list
+        Raster data of environment.
+    store : int 
+        Amount of food the agent currently has in its stomach.
+    agents : list
+        All agents in environment.
+    id : list
+        Agent's id number.
+    speed : int
+        How many spaces the agent will move in a single movement.
+               
+    Methods
+    -------
+    move()
+        Moves agent in y direction (up or down at random) and in x direction 
+        (left or right at random).
+    
+    distance_between(other_agent)
+        Calculates Euclidean distance between this agent and another agent.
+    """
+    def __init__(self, id, speed, environment, agents,
+                 x = None, y = None):
+        """
+        Constructor function for agent.
+        
+        Parameters
+        ----------
+        id : int 
+            Agent's id number.
+        speed : int
+            How many spaces the agent will move in a single movement.
+        environment : list
+            Raster data of environment.
+        agents : list
+            All agents in environment.
+        x : int
+            Agent's x co-ordinate. Defaults to None. If None, set as random x 
+            co-ordinate within the environment.
+        y : int
+            Agent's y co-ordinate. Defaults to None. If None, set as random y 
+            co-ordinate within the environment.
+        """
+        if x == None:
+            self._x = random.randint(0, len(environment[0]) - 1)
+        else:
+            self._x = x
+            
+        if y == None:
+            self._y = random.randint(0, len(environment) - 1)
+        else:
+            self._y = y
+            
+        self.environment = environment
+        self.store = 0 
+        self.agents = agents
+        self.id = id
+        self.speed = speed
+    
+    def __str__(self):
+        """
+        Print the id, x and y co-ordinates and the current store of the agent.
+        """
+        return "id: " + str(self.id) + ", " + \
+            "x: " + str(self.x) + ", " + \
+            "y: " + str(self.y) + ", " + \
+            "store: " + str(self.store)
+    
+    def get_x(self):
+        return self._x
+    
+    def set_x(self, value):
+        self._x = value
+        
+    def get_y(self):
+        return self._y
+    
+    def set_y(self, value):
+        self._y = value
+
+    def move(self):
+        """
+        Move agent in x direction and y direction (i.e. diagonally).
+        
+        Whether x and y co-ordinates are increased or decreased is decided at
+        random. The amount the agent moves is equal to its speed.
+        """
+        if random.random() < 0.5:
+            self._x = (self._x + self.speed) % len(self.environment[0])
+        else:
+            self._x = (self._x - self.speed) % len(self.environment[0])
+        
+        if random.random() < 0.5:
+            self._y = (self._y + self.speed) % len(self.environment)
+        else:
+            self._y = (self._y - self.speed) % len(self.environment)
+    
+    def distance_between(self, other_agent):
+        """
+        Measure Euclidean distance between this agent and another agent.
+        
+        Parameters
+        ----------
+        other_agent : agent
+            The agent to measure the distance from.
+        """
+        return ((self.x - other_agent.x)**2 + (self.y - other_agent.y)**2)**0.5
+             
+    x = property(get_x, set_x, "x property")
+    y = property(get_y, set_y, "y property")
+
+class Sheep(Agent):
+    """
+    Define an instance of a single sheep. Inherits Agent class.
+    
+    Methods
+    -------
+    eat()
+        Increase store by 10 and remove 10 units from environment at sheep's 
+        current co-ordinate.
+    
+    throw_up()
+        If store is over 100, add store amount to the environment at sheep's
+        current co-ordinate and set store to 0.
+    
+    share_with_neighbours(neighbourhood)
+        If there is an agent within the neighbourhood of this agent with a 
+        lower store, split the total store between the two agents.
+        
+    """
+    def eat(self):
+        """
+        Increase store by 10 and remove 10 units from environment at sheep's 
+        current co-ordinate.
+        
+        If there are fewer than 10 units remaining in the environment at the
+        surrent position, add the remaining number of units to store and set
+        the environment value to be 0.
+        """
+        if self.environment[self.y][self.x] > 10:
+            self.environment[self.y][self.x] -= 10
+            self.store += 10
+        elif self.environment[self.y][self.x] > 0:
+            self.store = self.environment[self.y][self.x]
+            self.environment[self.y][self.x] = 0
+        
+    def throw_up(self):
+        """
+        If store is over 100, add store amount to the environment at sheep's
+        current co-ordinate and set store to 0.
+        """
+        if self.store > 100:
+            self.environment[self.y][self.x] += self.store
+            self.store = 0   
+    
+    def share_with_neighbours(self, neighbourhood):
+        """
+        share_with_neighbours(neighbourhood)
+            If there is an agent within the neighbourhood of this sheep with a 
+            lower store, split the total store between the two agents.
+            
+            This is done by looping over agents, giving priority to those
+            earlier in the list of agents. This method calls distance_between
+            from the agent superclass.
+            
+            Parameters
+            ----------
+            neighbourhood : int
+                The distance within which to look for another agent.
+        """
+        #print(neighbourhood) - check the method works
+        # Loop through the agents in self.agents .
+        for agent in self.agents:
+            # Calculate the distance between self and the current other agent:
+            distance = self.distance_between(agent)
+            # If distance is less than or equal to the neighbourhood
+            if distance <= neighbourhood:
+                # Sum self.store and agent.store and halve to calculate average
+                average_store = (self.store + agent.store)/2
+                self.store = average_store
+                agent.store = average_store
+                
+class Wolf(Agent):
+    """
+    Define an instance of a single wolf. Inherits Agent class.
+    
+    Methods
+    -------
+    eat(neighbourhood)
+        If there is a sheep within the neighbourhood of this wolf, remove the 
+        sheep from the list of agents and add one to the wolf's store.
+    """
+    def eat(self, neighbourhood):
+        """
+        If there is a sheep within the neighbourhood of this wolf, remove the 
+        sheep from the list of agents and add one to the wolf's store.
+        
+        Will also print out a message that a sheep has been eaten and give the
+        sheep's details. This method inherits the __str__ and distance_between
+        methods from the agent superclass.
+        
+        Parameters
+        ----------
+        neighbourhood : int
+            The distance within which to look for a sheep.
+        """
+        distances = []
+        sheeps = [agent for agent in self.agents if isinstance(agent, Sheep)]
+        
+        if len(sheeps) > 0:
+            for sheep in sheeps:
+                distances.append(self.distance_between(sheep))
+            min_distance = min(enumerate(distances), key = operator.itemgetter(1))
+            closest_sheep = sheeps[min_distance[0]]
+            if min_distance[1] < neighbourhood:
+                self.x = closest_sheep.x
+                self.y = closest_sheep.y
+                self.store += 1
+                self.agents.remove(closest_sheep)
+                print("A sheep was eaten! \n " + str(closest_sheep))
+
+    
+       
---
+++ final_model.py
@@ -0,0 +1,204 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 14 12:33:27 2021
+
+@author: Tom Cunningham
+"""
+import matplotlib
+import matplotlib.animation
+import agentframework
+import csv
+import random
+import tkinter
+import requests
+import bs4
+
+# use tkinter backend
+matplotlib.use("TkAgg")
+
+#matplotlib.pyplot.ioff()
+
+# Get data for sheep xs and ys
+r = requests.get("https://www.geog.leeds.ac.uk/courses/computing/practicals/python/agent-framework/part9/data.html")
+# Check response is 200
+# print(r)
+
+# Parse HTML and read ys and xs
+soup = bs4.BeautifulSoup(r.text, "html.parser")
+td_ys = soup.find_all(attrs={"class" : "y"})
+td_xs = soup.find_all(attrs={"class" : "x"})
+
+# Create empty list for environment raster data
+environment = []
+
+# Read in CSV raster data
+with open('in.txt', newline='') as f:
+    reader = csv.reader(f, quoting=csv.QUOTE_NONNUMERIC)
+    for row in reader:
+        rowlist = []
+        for value in row: 
+            rowlist.append(value)
+        environment.append(rowlist)
+
+# Set number of sheep, wolves, random movements and size of neighbourhood
+num_of_sheeps = 200
+num_of_wolves = 5
+num_of_moves = 1000
+sheep_neighbourhood = 20
+wolf_neighbourhood = 30
+
+# Create empty list for sheep and wolves
+sheeps = []
+wolves = []
+
+# Draw plot
+fig = matplotlib.pyplot.figure(figsize = (7,7), frameon = False)
+#fig.add_axes([0, 0, 1, 1])
+
+# Create sheeps
+for i in range(num_of_sheeps):
+    
+    # Use data from web for xs and ys if it exists - after that use random int
+    if i < len(td_ys):
+        y = int(td_ys[i].text)
+    else:
+        y = None
+    
+    if i < len(td_xs):
+        x = int(td_xs[i].text)
+    else:
+        x = None
+    
+    sheeps.append(agentframework.Sheep(id = i,
+                                       x = x,
+                                       y = y,
+                                       environment = environment, 
+                                       agents = sheeps,
+                                       speed = 1))
+
+# Create wolves
+for i in range(num_of_wolves):
+    wolves.append(agentframework.Wolf(id = i, 
+                                      environment = environment, 
+                                      agents = sheeps,
+                                      speed = 3))
+
+# Create stopping condition for animation - runs if true
+carry_on = True
+
+# Define update function for animation
+def update(frame_number):
+      
+    global carry_on
+    fig.clear()
+    
+    # Move sheeps.
+    for i in range(len(sheeps)):
+        #print(sheeps[i])
+        random.shuffle(sheeps)
+        
+        sheeps[i].eat()
+        sheeps[i].throw_up()
+        sheeps[i].move()
+        sheeps[i].share_with_neighbours(sheep_neighbourhood)
+        
+    for i in range(len(wolves)):
+        #print(wolves[i])
+        random.shuffle(wolves)
+        
+        wolves[i].eat(wolf_neighbourhood)
+        wolves[i].move()
+        
+
+    # Plot sheeps
+    matplotlib.pyplot.imshow(environment)
+    for i in range(len(sheeps)):
+        matplotlib.pyplot.scatter(x = sheeps[i].x, y = sheeps[i].y,
+                                  color = "white"
+                                  )
+    
+    for i in range(len(wolves)):
+        matplotlib.pyplot.scatter(x = wolves[i].x, y = wolves[i].y,
+                                  color = "black"
+                                  )
+        matplotlib.pyplot.xlim(0, len(environment[0]))
+        matplotlib.pyplot.ylim(0, len(environment))
+
+    
+    
+    # Create list of all sheeps stores
+    #stores = [int(sheep.store) for sheep in sheeps]
+    #print(stores)
+    
+    """
+    # Update stopping condition if all sheeps have over 70 store
+    if (all(i > 70 for i in [sheep.store for sheep in sheeps])):
+        carry_on = False
+        print("stopping condition - all sheeps have over 70 store")
+    """
+    
+    # Update stopping condition if all the sheep have been eaten
+    if (len(sheeps) == 0):
+        carry_on = False
+        print("The wolves have won! All the sheep have been eaten!")
+    
+# Stop animation before max number of moves or if stopping condition met
+def gen_function():
+    global carry_on
+    i = 0
+    while (i < num_of_moves) & (carry_on):
+        yield i
+        i += 1
+    else:
+        carry_on = False
+        print("Sheep left: " + str(len(sheeps)) + "\n" +
+              "Survival rate: " + str(len(sheeps)/num_of_sheeps))
+        
+        # Write new environment
+        with open("out.txt", "w", newline = "") as f2:
+            writer = csv.writer(f2, delimiter = ",")
+            for row in environment:
+                writer.writerow(row)
+                
+        # Get list of sheeps current stores
+        sheep_stores = []
+        for sheep in sheeps:
+            sheep_stores.append(sheep.store)
+            
+        #print(sheep_stores)
+        
+        # Write sheeps current stores to a file
+        with open("sheep stores.txt", "a", newline = "") as f3:
+            writer = csv.writer(f3, delimiter = ",")
+            writer.writerow(sheep_stores)
+
+
+# Define function to pass to tkinter to run animation
+def run():
+    global animation
+    animation = matplotlib.animation.FuncAnimation(fig, update, interval=1, 
+                                                   repeat = False, 
+                                                   frames = gen_function())
+    canvas.draw()
+
+# Define function to pause the animation in tkinter    
+def pause():
+    try:
+        animation.event_source.stop()
+    except:
+        print("No animation")
+        
+root = tkinter.Tk()
+root.wm_title("Model")
+canvas = matplotlib.backends.backend_tkagg.FigureCanvasTkAgg(fig, master = root)
+canvas._tkcanvas.pack(side = tkinter.TOP, fill = tkinter.BOTH, expand = 1)
+menu_bar = tkinter.Menu(root)
+root.config(menu=menu_bar)
+model_menu = tkinter.Menu(menu_bar)
+menu_bar.add_cascade(label="Model", menu=model_menu)
+model_menu.add_command(label="Run model", command=run)
+model_menu.add_command(label = "Pause", command = pause)
+
+
+tkinter.mainloop()
+
